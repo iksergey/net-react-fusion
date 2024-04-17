256b59d0147e0c9c8583fdb3c7e87694749b09a4
Sergei Kamianetskii <i@ksergey.ru>
2024-04-17 13:52:31 +0300

    Подготовка представления

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,41 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div>
+      <div>
+        <div>
+          <h1>Список контактов</h1>
+        </div>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Имя контакта</th>
+                <th>E-mail</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th>1</th>
+                <th>Имя фамилия 1</th>
+                <th>example@email1.ru</th>
+              </tr>
 
+              <tr>
+                <th>2</th>
+                <th>Имя фамилия 2</th>
+                <th>example@email2.ru</th>
+              </tr>
+
+              <tr>
+                <th>3</th>
+                <th>Имя фамилия 3</th>
+                <th>example@email3.ru</th>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
