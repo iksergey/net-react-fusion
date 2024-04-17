2b395671d5f5f364e686f58d7cbeb135b0fcc0ed
Sergei Kamianetskii <i@ksergey.ru>
2024-04-17 18:03:50 +0300

    Настройка CORS и секретики

--- api/Program.cs
+++ api/Program.cs
@@ -13,9 +13,18 @@ builder.Services.AddSwaggerGen(opt =>
 builder.Services.AddControllers();
 builder.Services.AddSingleton<ContactStorage>();
 
+builder.Services.AddCors(opt =>
+opt.AddPolicy("CorsPolicy", policy =>
+{
+    policy.AllowAnyMethod()
+    .AllowAnyHeader()
+    .WithOrigins(args[0]);
+}));
+
 var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.MapControllers();
+app.UseCors("CorsPolicy");
 app.Run();
