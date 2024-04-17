0bbb765baf976cc8e2234b48d5891a1ef6aea800
Sergei Kamianetskii <i@ksergey.ru>
2024-04-17 12:52:26 +0300

    Инициализация проекта

---
+++ api/Program.cs
@@ -0,0 +1,21 @@
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "API списка контактов",
+    });
+});
+builder.Services.AddControllers();
+builder.Services.AddSingleton<ContactStorage>();
+
+var app = builder.Build();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.MapControllers();
+app.Run();
