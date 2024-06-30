6fadcedde3589cce12f3923a919c7b1d6c2dab27
Sergei Kamianetskii <i@ksergey.ru>
2024-06-30 04:12:36 +0300

    Метод расширения для IServiceCollection

--- api/Program.cs
+++ api/Program.cs
@@ -1,26 +1,5 @@
-using Microsoft.OpenApi.Models;
-
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(opt =>
-{
-    opt.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "API списка контактов",
-    });
-});
-builder.Services.AddControllers();
-var stringConnection = builder.Configuration.GetConnectionString("SqliteStringConnection");
-builder.Services.AddSingleton<IStorage>(new SqliteStorage(stringConnection));
-
-builder.Services.AddCors(opt =>
-opt.AddPolicy("CorsPolicy", policy =>
-{
-    policy.AllowAnyMethod()
-    .AllowAnyHeader()
-    .WithOrigins(builder.Configuration["client"]);
-}));
+builder.Services.AddServiceCollection(builder.Configuration);
 
 var app = builder.Build();
 app.UseSwagger();
