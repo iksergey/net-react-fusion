b0a4d755db7d432bcd200eb4024e14ac8513399c
Sergei Kamianetskii <i@ksergey.ru>
2024-07-06 05:03:08 +0300

    Пагинация на сервере

--- api/Storage/IPaginationStorage.cs
+++ api/Storage/IPaginationStorage.cs
@@ -1,4 +1,5 @@
 public interface IPaginationStorage : IStorage
 {
     Contact GetContactById(int id);
+    (List<Contact>, int TotalCount) GetContacts(int pageNumber, int pageSize);
 }
--- api/Storage/SqlitePaginationEfStorage.cs
+++ api/Storage/SqlitePaginationEfStorage.cs
@@ -1,3 +1,4 @@
+
 public class SqlitePaginationEfStorage : SqliteEfStorage, IPaginationStorage
 {
     public SqlitePaginationEfStorage(SqliteDbContext context)
@@ -8,5 +9,15 @@ public class SqlitePaginationEfStorage : SqliteEfStorage, IPaginationStorage
     public Contact GetContactById(int id)
     {
         return base.context.Contacts.Find(id);
+    }
+
+    public (List<Contact>, int TotalCount) GetContacts(int pageNumber, int pageSize)
+    {
+        int total = base.context.Contacts.Count();
+        List<Contact> contacts = base.context.Contacts
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .ToList();
+        return (contacts, total);
     }
 }
