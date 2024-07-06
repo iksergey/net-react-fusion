d9d34792da4320d63e7843104430d9a31a9f8ea1
Sergei Kamianetskii <i@ksergey.ru>
2024-07-06 16:02:09 +0300

    Подготовка к созданию пагинации

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,30 @@ import TableContact from "./layout/TableContact/TableContact";
 import FormContact from "./layout/FormContact/FormContact";
 import { Route, Routes, useLocation } from 'react-router-dom';
 import ContactDetails from './layout/ContactDetails/ContactDetails';
+import Pagination from './layout/Pagination/Pagination';
 
 const baseApiUrl = process.env.REACT_APP_API_URL;
 
 const App = () => {
     const [contacts, setContacts] = useState([]);
     const location = useLocation();
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+    const [pageSize] = useState(10);
+
+    const handlePageChange = (pageNumber) => {
+        setCurrentPage(pageNumber);
+    }
 
     useEffect(() => {
-        const url = `${baseApiUrl}/contacts`;
+        const url = `${baseApiUrl}/contacts/page?pageNumber=${currentPage}&pageSize=${pageSize}`;
         axios.get(url).then(
-            res => setContacts(res.data)
+            res => {
+                setContacts(res.data.contacts);
+                setTotalPages(Math.ceil(res.data.totalCount / pageSize))
+            }
         );
-    }, [location.pathname]);
+    }, [currentPage, pageSize, location.pathname]);
 
     const addContact = (contactName, contactEmail) => {
         const item = {
@@ -24,9 +35,15 @@ const App = () => {
             email: contactEmail
         };
 
-        const url = `${baseApiUrl}/contacts`;
-        axios.post(url, item).then(
-            response => setContacts([...contacts, response.data])
+        let url = `${baseApiUrl}/contacts`;
+        axios.post(url, item);
+
+        url = `${baseApiUrl}/contacts/page?pageNumber=${currentPage}&pageSize=${pageSize}`;
+        axios.get(url).then(
+            res => {
+                setContacts(res.data.contacts);
+                setTotalPages(Math.ceil(res.data.totalCount / pageSize))
+            }
         );
     }
 
@@ -42,6 +59,11 @@ const App = () => {
                         <div className="card-body">
                             <TableContact
                                 contacts={contacts}
+                            />
+                            <Pagination
+                                currentPage={currentPage}
+                                totalPages={totalPages}
+                                onPageChange={handlePageChange}
                             />
                             <FormContact addContact={addContact} />
 
---
+++ client/src/layout/Pagination/Pagination.jsx
@@ -0,0 +1,7 @@
+const Pagination = () => {
+    return (
+        <>1 2 3 4 ... 10</>
+    );
+}
+
+export default Pagination;
