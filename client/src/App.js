f6838d5b370131e37dc68ad14b851170cfc33c31
Sergei Kamianetskii <i@ksergey.ru>
2024-07-06 18:09:06 +0300

    Страница добавления контакта

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import React, { useState, useEffect } from "react";
 import TableContact from "./layout/TableContact/TableContact";
-import FormContact from "./layout/FormContact/FormContact";
-import { Route, Routes, useLocation } from 'react-router-dom';
+import AppendContact from "./layout/FormContact/AppendContact";
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import ContactDetails from './layout/ContactDetails/ContactDetails';
 import Pagination from './layout/Pagination/Pagination';
 
@@ -29,24 +29,6 @@ const App = () => {
         );
     }, [currentPage, pageSize, location.pathname]);
 
-    const addContact = (contactName, contactEmail) => {
-        const item = {
-            name: contactName,
-            email: contactEmail
-        };
-
-        let url = `${baseApiUrl}/contacts`;
-        axios.post(url, item);
-
-        url = `${baseApiUrl}/contacts/page?pageNumber=${currentPage}&pageSize=${pageSize}`;
-        axios.get(url).then(
-            res => {
-                setContacts(res.data.contacts);
-                setTotalPages(Math.ceil(res.data.totalCount / pageSize))
-            }
-        );
-    }
-
     return (
         <div className="container mt-5">
             <Routes>
@@ -65,12 +47,15 @@ const App = () => {
                                 totalPages={totalPages}
                                 onPageChange={handlePageChange}
                             />
-                            <FormContact addContact={addContact} />
-
+                            <Link to="/append"
+                                className="btn btn-success mt-3">
+                                Добавить контакт
+                            </Link>
                         </div>
                     </div>
                 } />
                 <Route path="contact/:id" element={<ContactDetails />} />
+                <Route path="append" element={<AppendContact />} />
             </Routes>
         </div>
     );
